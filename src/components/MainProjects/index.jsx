a11e56bfda01e14e9ec9c209a1b1681882ba1dd4
Lucas-PC <lgbs@cesar.school>
2024-08-13 20:42:19 -0300

    remove pagination from carrousel

--- src/components/MainProjects/index.jsx
+++ src/components/MainProjects/index.jsx
@@ -1,10 +1,20 @@
 import Container from "../Container";
+import ProjectCard from "../ProjectCard";
+
+const project = {
+  name: "Project Name",
+  link: "https://www.github.com",
+  description: "Project Description",
+};
+
 
 function MainProjects(){
   return (
     <Container className={"bg-zinc-600"}>
       <h2 className="text-5xl font-OleoScript text-emerald-600 sm:text-8xl">Projects</h2>
-      
+
+      <ProjectCard project={project}/>
+
     </Container>
   );
 }
