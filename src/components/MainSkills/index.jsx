03b5c312773038d2d1a440229fd08f8c1ed1db6b
Lucas-PC <lgbs@cesar.school>
2024-08-13 01:14:07 -0300

    tools and languages in carrousel

--- src/components/MainSkills/index.jsx
+++ src/components/MainSkills/index.jsx
@@ -1,3 +1,10 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y, Autoplay } from 'swiper/modules';
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay'
+
 function MainSkills() {
   const skills = [
     {
@@ -24,29 +31,54 @@ function MainSkills() {
       name: "Django",
       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg",
     },
+    {
+      name: "StreamLit",
+      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/streamlit/streamlit-original.svg"
+    },
+    {
+      name: "MySql",
+      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg"
+    },
+    {
+      name: "Arduino",
+      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/arduino/arduino-original.svg"
+    },
+    {
+      name: "Railway",
+      icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/railway/railway-original-wordmark.svg"
+    }
   ];
 
   return (
-    <div className="bg-indigo-700 flex items-center justify-center py-16 flex-col gap-y-32">
+    <div className="bg-slate-400 flex items-center justify-center py-16 flex-col h-screen gap-10">
       <div>
-        <h2 className="text-2xl font-Lobster text-white sm:text-5xl">
+        <h2 className="text-2xl font-Lobster text-cyan-950 sm:text-5xl">
           Top Languages and Tools
         </h2>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-16 sm:gap-32 content-evenly">
+      
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={4}
+        modules={[Navigation, Pagination, A11y, Autoplay]}
+        autoplay={{ delay: 2000 }}
+        navigation
+        pagination={{ clickable: true }}
+        a11y={{ enabled: true }}
+        rewind={true}
+        className="w-full"
+
+      >
         {skills.map((skill, index) => (
-          <div key={index} className="w-full flex flex-col items-center">
-            <img
-              src={skill.icon}
-              alt={skill.name}
-              className="w-60 h-60"
-            />
-            <span className="text-white font-Lobster text-lg sm:text-xl mt-2">
-              {skill.name}
-            </span>
-          </div>
+          <SwiperSlide key={index}>
+            <div className="flex flex-col items-center gap-y-4 select-none my-12">
+              <img src={skill.icon} alt={skill.name} className="w-60 h-60" />
+              <p className="text-white font-bold text-lg">{skill.name}</p>
+            </div>
+          </SwiperSlide>
         ))}
-      </div>
+      </Swiper>
+
     </div>
   );
 }
