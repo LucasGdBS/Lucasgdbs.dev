19ed4ed33e77c4d77f796257f2aaef0a73d01507
Lucas-PC <lgbs@cesar.school>
2024-08-12 21:48:07 -0300

    create MainBanner

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Lobster': ['Lobster', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
