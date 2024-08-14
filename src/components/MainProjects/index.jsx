848faf723c6e5d75ebcbcf6eefa5e5b7f8474815
Lucas-PC <lgbs@cesar.school>
2024-08-13 22:11:33 -0300

    addin carrousel

--- src/components/MainProjects/index.jsx
+++ src/components/MainProjects/index.jsx
@@ -1,20 +1,33 @@
 import Container from "../Container";
-import ProjectCard from "../ProjectCard";
-
-const project = {
-  name: "Project Name",
-  link: "https://www.github.com",
-  description: "Project Description",
-};
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, A11y } from "swiper/modules";
+import 'swiper/css';
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 
-function MainProjects(){
+function MainProjects() {
   return (
     <Container className={"bg-zinc-600"}>
-      <h2 className="text-5xl font-OleoScript text-emerald-600 sm:text-8xl">Projects</h2>
+      <h2 className="text-5xl font-OleoScript text-emerald-600 sm:text-8xl">
+        Projects
+      </h2>
 
-      <ProjectCard project={project}/>
-
+      <Swiper
+        direction={"vertical"}
+        slidesPerView={1}
+        modules={[Autoplay, Pagination, A11y]}
+        autoplay={{ delay: 2000 }}
+        a11y={{ enabled: true }}
+        loop={true}
+        pagination={{ clickable: false }}
+        className="w-full sm:w-4/5"
+      >
+        <SwiperSlide className="bg-black">Slide 1</SwiperSlide>
+        <SwiperSlide className="bg-red-500">Slide 2</SwiperSlide>
+        <SwiperSlide className="bg-orange-500">Slide 3</SwiperSlide>
+        <SwiperSlide className="bg-sky-600">Slide 4</SwiperSlide>
+      </Swiper>
     </Container>
   );
 }
